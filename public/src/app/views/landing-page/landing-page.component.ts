e58a2429f1b83e52756e003ea4dd24a71b19a5fa
Matthew Grossman <mattig@udel.edu>
2019-11-20 14:50:26 -0500

    fixed some firebase compat issues

--- public/src/app/views/landing-page/landing-page.component.ts
+++ public/src/app/views/landing-page/landing-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase/app';
+import * as firebase from '@firebase/app';
 import { AuthService } from '../../services/auth.service';
 
 @Component({
